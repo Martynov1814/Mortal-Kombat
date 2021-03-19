f49be5e78c7a0753d65296df1d0e62182b5e775d
Martynov1814 <martynov_mk@mail.ru>
2021-03-19 12:35:58 +0300

    delit the link on picture

--- js/main.js
+++ js/main.js
@@ -3,6 +3,5 @@ $(document).ready(function () {
         mp4: 'video/video.mp4',
         webm: 'video/video.ogv',
         ogv: 'video/video.webm',
-        poster: 'img/cover-image.jpg'
     });
 });
